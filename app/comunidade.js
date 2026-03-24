128ffa064c019c733b54f746973bd115de47546f
Lucaseiki <lucaseiki02@gmail.com>
2026-03-24 09:32:42 -0300

    feat: adiciona tela de avisos com lista mockada e estados visuais

---
+++ app/comunidade.js
@@ -0,0 +1,10 @@
+import PlaceholderScreen from '../components/PlaceholderScreen';
+
+export default function ComunidadeScreen() {
+  return (
+    <PlaceholderScreen
+      title="Comunidade"
+      description="Tela reservada para os posts da comunidade."
+    />
+  );
+}
